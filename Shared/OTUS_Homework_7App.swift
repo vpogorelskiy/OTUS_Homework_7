cf902e28dd0cec1f42910b727557b2f2ac32594e
Vyacheslav Pogorelskiy <vyacheslav.pogorelskiy@gmail.com>
2021-11-27 17:46:46 +0300

    Initial commit

---
+++ Shared/OTUS_Homework_7App.swift
@@ -0,0 +1,17 @@
+//
+//  OTUS_Homework_7App.swift
+//  Shared
+//
+//  Created by Вячеслав Погорельский on 27.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct OTUS_Homework_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
